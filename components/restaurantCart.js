9f4fdf9b4c38c988777fe002d6c2358f3b72b12d
Falak sher <thisisfalaksher@gmial.com>
2025-06-13 10:05:10 -0700

    Now I complete my app

--- components/restaurantCart.js
+++ components/restaurantCart.js
@@ -3,6 +3,7 @@ import React from 'react'
 import * as Icon from "react-native-feather";
 import { themeColor } from '../theme';
 import { useNavigation, useRoute } from '@react-navigation/native';
+
 import { router } from 'expo-router';
 
 
--- constants/index.js
+++ constants/index.js
@@ -6,15 +6,15 @@ export const catagories=[
    },
     { id:2,
     name: 'Buger',
-    image:require('../assets/images/burger.jpeg'),
+    image:require('../assets/images/Chiken.jpeg'),
    },
     { id:3,
     name: 'Italian',
-    image:require('../assets/images/burger.jpeg'),
+    image:require('../assets/images/Chorma.jpeg'),
    },
     { id:4,
     name: 'Noodles',
-    image:require('../assets/images/burger.jpeg'),
+    image:require('../assets/images/fish.png'),
    },
     { id:5,
     name: 'Chines',
@@ -34,7 +34,7 @@ export const featured=
         {
             id:1,
             name:'papa johen',
-            image: require('../assets/images/burger.jpeg'),
+            image: require('../assets/images/fish.png'),
             description:'Hot and spicy pizza',
             lng:38.2145402,
             lat:-85.5324249,
@@ -44,35 +44,176 @@ export const featured=
             catogory:'fast food',
             dishes:[
                 {
-                    id:1,
-                    name:'Pizza',
-                    description:'cheezy and garlic pizza',
-                    price:10,
-                    image: require('../assets/images/burger.jpeg')
-
-                },
-                {
                     id:2,
-                    name:'Pizza',
+                    name:'Chiken',
                     description:'cheezy and garlic pizza',
                     price:10,
-                    image: require('../assets/images/burger.jpeg')
+                    image: require('../assets/images/Chiken.jpeg')
 
                 },
                 {
                     id:3,
-                    name:'Pizza',
-                    description:'cheezy and garlic pizza',
+                    name:'Chorma',
+                    description:'Hot and spicy',
                     price:10,
-                    image: require('../assets/images/burger.jpeg')
+                    image: require('../assets/images/Chorma.jpeg')
 
                 },
                 {
                     id:4,
-                    name:'Pizza',
+                    name:'KFC',
+                    description:'Delicious and spicy',
+                    price:10,
+                    image: require('../assets/images/kfc.webp')
+
+                },
+                {
+                    id:5,
+                    name:'Rules',
+                    description:'What a tast of this food',
+                    price:10,
+                    image: require('../assets/images/Rules.jpeg')
+
+                },
+
+            ]
+        },
+         {
+            id:2,
+            name:'Falak',
+            image: require('../assets/images/beef.jpg'),
+            description:'Hot and spicy pizza',
+            lng:38.2145402,
+            lat:-85.5324249,
+            address:'434 second street',
+            stars:4,
+            reviews:'4.48',
+            catogory:'fast food',
+            dishes:[
+                {
+                    id:2,
+                    name:'Chiken',
                     description:'cheezy and garlic pizza',
                     price:10,
-                    image: require('../assets/images/burger.jpeg')
+                    image: require('../assets/images/Chiken.jpeg')
+
+                },
+                {
+                    id:3,
+                    name:'Chorma',
+                    description:'Hot and spicy',
+                    price:10,
+                    image: require('../assets/images/Chorma.jpeg')
+
+                },
+                {
+                    id:4,
+                    name:'KFC',
+                    description:'Delicious and spicy',
+                    price:10,
+                    image: require('../assets/images/kfc.webp')
+
+                },
+                {
+                    id:5,
+                    name:'Rules',
+                    description:'What a tast of this food',
+                    price:10,
+                    image: require('../assets/images/Rules.jpeg')
+
+                },
+
+            ]
+        },
+         {
+            id:3,
+            name:'Sher',
+            image: require('../assets/images/Chorma.jpeg'),
+            description:'Hot and spicy pizza',
+            lng:38.2145402,
+            lat:-85.5324249,
+            address:'434 second street',
+            stars:4,
+            reviews:'4.48',
+            catogory:'fast food',
+            dishes:[
+                {
+                    id:4,
+                    name:'Chiken',
+                    description:'cheezy and garlic pizza',
+                    price:10,
+                    image: require('../assets/images/Chiken.jpeg')
+
+                },
+                {
+                    id:3,
+                    name:'Chorma',
+                    description:'Hot and spicy',
+                    price:10,
+                    image: require('../assets/images/Chorma.jpeg')
+
+                },
+                {
+                    id:4,
+                    name:'KFC',
+                    description:'Delicious and spicy',
+                    price:10,
+                    image: require('../assets/images/kfc.webp')
+
+                },
+                {
+                    id:5,
+                    name:'Rules',
+                    description:'What a tast of this food',
+                    price:10,
+                    image: require('../assets/images/Rules.jpeg')
+
+                },
+
+            ]
+        },
+         {
+            id:5,
+            name:'Fareed',
+            image: require('../assets/images/Chiken.jpeg'),
+            description:'Hot and spicy pizza',
+            lng:38.2145402,
+            lat:-85.5324249,
+            address:'434 second street',
+            stars:4,
+            reviews:'4.48',
+            catogory:'fast food',
+            dishes:[
+                {
+                    id:2,
+                    name:'Chiken',
+                    description:'cheezy and garlic pizza',
+                    price:10,
+                    image: require('../assets/images/Chiken.jpeg')
+
+                },
+                {
+                    id:3,
+                    name:'Chorma',
+                    description:'Hot and spicy',
+                    price:10,
+                    image: require('../assets/images/Chorma.jpeg')
+
+                },
+                {
+                    id:4,
+                    name:'KFC',
+                    description:'Delicious and spicy',
+                    price:10,
+                    image: require('../assets/images/kfc.webp')
+
+                },
+                {
+                    id:5,
+                    name:'Rules',
+                    description:'What a tast of this food',
+                    price:10,
+                    image: require('../assets/images/Rules.jpeg')
 
                 },
 
